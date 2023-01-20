87b5b19a24401eb008b0a3e85f000d6c9b51b5b4
Daniel Emery <daniel.emery@abusix.com>
2023-01-20 14:57:27 +0100

    Test multiple cloudfronts

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ resource "aws_cloudfront_origin_access_identity" "aws_multi_spa" {
   comment = "aws_multi_spa"
 }
 
+locals {
+  s3_origin_id = "aws-multi-spa-origin"
+  all_buckets  = merge(aws_s3_bucket.applications, { "application_fallback" = aws_s3_bucket.application_fallback })
+}
+
 data "aws_iam_policy_document" "read_aws_multi_spa_bucket" {
   for_each = aws_s3_bucket.applications
 
@@ -26,9 +31,39 @@ data "aws_iam_policy_document" "read_aws_multi_spa_bucket" {
   }
 }
 
-locals {
-  s3_origin_id = "aws-multi-spa-origin"
+data "aws_iam_policy_document" "read_aws_multi_spa_bucket_fallback" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.application_fallback.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.aws_multi_spa.iam_arn]
+    }
+  }
+
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.application_fallback.arn]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.aws_multi_spa.iam_arn]
+    }
+  }
 }
+
+resource "aws_s3_bucket_policy" "read_aws_multi_spa_bucket" {
+  bucket = aws_s3_bucket.application_fallback.id
+  policy = data.aws_iam_policy_document.read_aws_multi_spa_bucket_fallback.json
+}
+
+# resource "aws_iam_policy" "read_aws_multi_spa_bucket" {
+#   for_each = data.aws_iam_policy_document.read_aws_multi_spa_bucket
+#   name = "${each.key}-read_aws_multi_spa_bucket"
+#   path = "/"
+#   policy = each.value.json
+# }
 
 resource "aws_s3_bucket" "applications" {
   for_each = {
@@ -42,62 +77,44 @@ resource "aws_s3_bucket" "applications" {
 }
 
 resource "aws_s3_bucket" "application_fallback" {
-  for_each = {
-    application-fallback = "application-fallback"
-  }
-  bucket = each.key
+  bucket = "application-fallback"
   tags = {
     "index_folder" = "fallback"
   }
 }
+# resource "aws_s3_bucket_policy" "public" {
+#   for_each = local.all_buckets
 
-resource "aws_s3_bucket_policy" "public" {
-  for_each = merge(aws_s3_bucket.applications, aws_s3_bucket.application_fallback)
+#   bucket = each.value.id
+#   policy = jsonencode({
+#     "Version" : "2012-10-17",
+#     "Statement" : [
+#       {
+#         "Sid" : "PublicReadGetObject",
+#         "Effect" : "Allow",
+#         "Principal" : "*",
+#         "Action" : "s3:GetObject",
+#         "Resource" : "${each.value.arn}/*",
+#       }
+#     ]
+#   })
+# }
 
-  bucket = each.value.id
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Sid" : "PublicReadGetObject",
-        "Effect" : "Allow",
-        "Principal" : "*",
-        "Action" : "s3:GetObject",
-        "Resource" : "${each.value.arn}/*",
-      }
-    ]
-  })
-}
+resource "aws_cloudfront_distribution" "s3_distribution_fallback" {
 
-resource "aws_s3_bucket_website_configuration" "website_bucket_configuration" {
-  for_each = merge(aws_s3_bucket.applications, aws_s3_bucket.application_fallback)
-
-  bucket = each.value.bucket
-
-  index_document {
-    suffix = "index.html"
+  aliases = ["multi-spa.hermanns.pro"]
+  viewer_certificate {
+    acm_certificate_arn            = "arn:aws:acm:us-east-1:880809221583:certificate/5b9acdcb-a338-49a3-9e82-57c31fbfee13"
+    cloudfront_default_certificate = false
+    minimum_protocol_version       = "TLSv1.2_2021"
+    ssl_support_method             = "sni-only"
   }
 
-  error_document {
-    key = "${each.value.tags_all.index_folder}/index.html"
-  }
-}
-
-
-resource "aws_cloudfront_distribution" "s3_distribution" {
-
-  dynamic "origin" {
-    for_each = aws_s3_bucket_website_configuration.website_bucket_configuration
-    content {
-      domain_name = origin.value.website_endpoint
-      origin_id   = origin.key
-
-      custom_origin_config {
-        http_port              = 80
-        https_port             = 443
-        origin_protocol_policy = "http-only"
-        origin_ssl_protocols   = ["SSLv3"]
-      }
+  origin {
+    domain_name = aws_s3_bucket.application_fallback.bucket_domain_name
+    origin_id   = aws_s3_bucket.application_fallback.bucket_domain_name
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.aws_multi_spa.cloudfront_access_identity_path
     }
   }
 
@@ -112,29 +129,10 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   is_ipv6_enabled     = true
   default_root_object = "index.html"
 
-  dynamic "ordered_cache_behavior" {
-    for_each = aws_s3_bucket.applications
-    content {
-      allowed_methods        = ["GET", "HEAD", "OPTIONS"]
-      cached_methods         = ["GET", "HEAD"]
-      target_origin_id       = ordered_cache_behavior.key
-      path_pattern           = "/${ordered_cache_behavior.value.tags_all.index_folder}*"
-      viewer_protocol_policy = "allow-all"
-
-      forwarded_values {
-        query_string = false
-
-        cookies {
-          forward = "none"
-        }
-      }
-    }
-  }
-
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "application-fallback"
+    target_origin_id = aws_s3_bucket.application_fallback.bucket_domain_name
 
     forwarded_values {
       query_string = false
@@ -150,7 +148,86 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     max_ttl                = 86400
   }
 
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/${aws_s3_bucket.application_fallback.tags_all.index_folder}/index.html"
+  }
+}
+
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  for_each = aws_s3_bucket.applications
+
+  aliases = ["multi-spa.hermanns.pro"]
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn            = "arn:aws:acm:us-east-1:880809221583:certificate/5b9acdcb-a338-49a3-9e82-57c31fbfee13"
+    cloudfront_default_certificate = false
+    minimum_protocol_version       = "TLSv1.2_2021"
+    ssl_support_method             = "sni-only"
+  }
+
+  origin {
+    domain_name = each.value.bucket_domain_name
+    origin_id   = each.key
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.aws_multi_spa.cloudfront_access_identity_path
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["DE"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = each.key
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/${each.value.tags_all.index_folder}/index.html"
+  }
+}
+
+data "cloudflare_zone" "website_zone" {
+  name = "hermanns.pro"
+}
+
+resource "cloudflare_record" "website_record" {
+  zone_id = data.cloudflare_zone.website_zone.id
+  name    = "multi_spa"
+  value   = aws_cloudfront_distribution.s3_distribution_fallback.domain_name
+  type    = "CNAME"
+  proxied = true
+}
+
+resource "cloudflare_page_rule" "website_flexible_ssl" {
+  zone_id = data.cloudflare_zone.website_zone.id
+  target  = "multi_spa.hermanns.pro/*"
+  actions {
+    ssl = "flexible"
   }
 }
--- providers.tf
+++ providers.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.16.0"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
     doppler = {
       source  = "DopplerHQ/doppler"
       version = "1.1.1"
@@ -23,6 +27,9 @@ provider "aws" {
   secret_key = data.doppler_secrets.this.map.AWS_SECRET_ACCESS_KEY
 }
 
+provider "cloudflare" {
+  api_token = data.doppler_secrets.this.map.CLOUDFLARE_API_TOKEN
+}
 
 provider "doppler" {
   doppler_token = var.doppler_service_token
